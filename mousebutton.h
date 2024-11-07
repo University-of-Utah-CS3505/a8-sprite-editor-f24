0b8dcb8a788c8c87ad22566dc498ead0231809d1
kogasaa <92961177+kogasaa@users.noreply.github.com>
2024-11-06 23:38:35 -0700

    add comments in the button press class

--- mousebutton.h
+++ mousebutton.h
@@ -2,6 +2,10 @@
 #define MOUSEBUTTON_H
 #include <QMouseEvent>
 
+/**
+ * @brief The buttonType enum
+ *        all the mouse actions can be used as enum
+ */
 enum buttonType{
     leftButtonUp,
     leftButtonDown,
@@ -11,15 +15,41 @@ enum buttonType{
     mouseMove
 };
 
+/**
+ * @brief The MouseButton class
+ * this class is a class to describe the mouse press action, like click scroll, or other elese
+ * we will record where it happened and how many times it proceed.
+ */
 class MouseButton
 {
     buttonType buttonType;
     QPointF pos;
     int amount;
 public:
+    /**
+     * @brief MouseButton constructor of mouse button
+     * @param btn what type action it is
+     * @param pos the position of this mouse consor
+     * @param amount how many times this mouse action did
+     */
     MouseButton(enum buttonType btn, QPointF pos, int amount);
+
+    /**
+     * @brief getButtonType to get the type of button action
+     * @return enum, name of the button action
+     */
     const enum buttonType& getButtonType() const;
+
+    /**
+     * @brief getAmount, get how many times this button action done
+     * @return int the number of the action
+     */
     int getAmount() const;
+
+    /**
+     * @brief getPos, get the position of this mouse action
+     * @return QPointF as the relative position of this action.
+     */
     QPointF getPos() const;
 };
 
