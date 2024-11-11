a84a231b64f1409278933abbfdd9c6b7f0e870cf
Ping880917 <anson880917@gmail.com>
2024-11-10 17:32:22 -0700

    finish the imagetoll cpp and h file
    finished the three functions without the printed load image

--- imagetool.cpp
+++ imagetool.cpp
@@ -1,3 +1,64 @@
 #include "imagetool.h"
+#include "qpainter.h"
+#include <QDebug>
 
 ImageTool::ImageTool() {}
+
+void ImageTool::flipImage(QImage &image, bool isVerticalFlip)
+{
+    // checking image is existed or not.
+    if(image.isNull())
+    {
+        return;
+    }
+
+    //using boolean isVerticalFlip to check whether we try to flip vertical
+    //or horizontal by using mirrored function.
+    if (isVerticalFlip)
+    {
+        image = image.mirrored(false, true);
+    }
+    else
+    {
+        image = image.mirrored(true, false);
+    }
+
+}
+
+
+void ImageTool::rotateImage(QImage &image)
+{
+    if(image.isNull())
+    {
+        return;
+    }
+
+    // Using QTransform to rotate 90 degrees clockwise
+    QTransform rotateImage;
+    rotateImage.rotate(90);
+    image = image.transformed(rotateImage);
+
+}
+
+
+void ImageTool::fillBlankWithColor(QImage &image, QColor color)
+{
+    if(image.isNull())
+    {
+        return;
+    }
+
+    // go over each pixel and change the color by using setpixelcolor.
+    for(int x = 0; x < image.width(); x++)
+    {
+        for (int y = 0; y < image.height(); y++)
+        {
+            QColor pixel = image.pixelColor(x, y);
+            if(pixel.alpha() == 0)
+            {
+                image.setPixelColor(x, y, color);
+            }
+        }
+    }
+
+}
--- imagetool.h
+++ imagetool.h
@@ -7,9 +7,29 @@
 class ImageTool
 {
 public:
+    ///
+    /// \brief ImageTool: default constructor
+    ///
     ImageTool();
+
+    ///
+    /// \brief flipImage: flip the image vertical or horzontal
+    /// \param image: The QImage to be flipped
+    /// \param isVerticalFlip: check whether is vertical or horizontal.
+    ///
     void static flipImage(QImage& image, bool isVerticalFlip);
+
+    ///
+    /// \brief rotateImage: rotate the image 90 degrees once
+    /// \param image: The image that be rotated
+    ///
     void static rotateImage(QImage& image);
+
+    ///
+    /// \brief fillBlankWithColor: fill the blank color for the image
+    /// \param image: The image that being filled color
+    /// \param color: the color that we want to fill
+    ///
     void static fillBlankWithColor(QImage& image, QColor color);
 };
 
